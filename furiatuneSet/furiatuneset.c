eef006a60ee542350e56045d9db230b70fb7b46e
emarti <dtemarti@gmail.com>
2017-09-01 16:37:02 +0300

    Update furiatuneset.c

--- furiatuneSet/furiatuneset.c
+++ furiatuneSet/furiatuneset.c
@@ -1,3 +1,27 @@
+/*
+This file is part of FuriatuneGUI.
+FuriatuneSET by emarti, Murat Özdemir (C) 2017
+
+FuriatuneSET is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+FuriatuneSET is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with FuriatuneSET.  If not, see <http://www.gnu.org/licenses/>.
+
+Version: 1.0 01092017
+Target OS: AmigaOS2.X/3.X
+
+Web: https://github.com/emartisoft
+email: dtemarti@gmail.com
+*/
+
 #include "furiatuneset.h"
 
 // furiatune outputs
